0e520c3503bd413ad58567867fcf984ae901a51e
joaovitoramaral27 <otthersfunec@gmail.com>
2026-03-25 13:49:14 -0300

    Calculadora de Corrente Elétrica V2
    Added Loop

--- cdg1.cpp
+++ cdg1.cpp
@@ -2,21 +2,50 @@
 using namespace std;
 
 int main() {
+	int Cont = 0; {};
 	float tensao;
 	float resistencia;
-	cout << "Defina a Tensao" << endl;
-	cin >> tensao;
-	system("cls");
-	cout << "Defina a Resistencia" << endl;
-	cin >> resistencia;
-	system("cls");
-	float corrente(tensao / resistencia);
-	if (corrente > 1500) {
-		cout << "Corrente de Alta Tensao Detectada: " << corrente << endl;
-	} else if (corrente == 0) {
-		cout << "Corrente Inesistente" << endl;
-	} else {
-		cout << "Corrente de Baixa Tensao Detectada: " << corrente << endl;
-	}
-	return 0;
+		cout << "Defina a Tensao" << endl;
+		cin >> tensao;
+		system("cls");
+		cout << "Defina a Resistencia" << endl;
+		cin >> resistencia;
+		system("cls");
+		float corrente(tensao / resistencia);
+		if (corrente > 1500) {
+			cout << "Corrente de Alta Tensao Detectada: " << corrente << endl;
+		}
+		else if (corrente == 0) {
+			cout << "Corrente Inesistente" << endl;
+		}
+		else {
+			cout << "Corrente de Baixa Tensao Detectada: " << corrente << endl;
+		}
+		char confirm;
+		cout << "Continuar (S)" << endl;
+		cin >> confirm;
+		if (confirm == 's'){
+			Cont = 1;
+		}
+		system("cls");
+		while (Cont > 0) {
+			cout << "Defina a Tensao" << endl;
+			cin >> tensao;
+			system("cls");
+			cout << "Defina a Resistencia" << endl;
+			cin >> resistencia;
+			system("cls");
+			float corrente(tensao / resistencia);
+			if (corrente > 1500) {
+				cout << "Corrente de Alta Tensao Detectada: " << corrente << endl;
+			}
+			else if (corrente == 0) {
+				cout << "Corrente Inesistente" << endl;
+			}
+			else {
+				cout << "Corrente de Baixa Tensao Detectada: " << corrente << endl << endl;
+			}
+		}
+		return 0;
+
 }
